2e1ec15483eae7d14921ce0ae2298538b2aa26e6
TimDev14 <njagi.timothy14@gmail.com>
2026-07-06 08:50:06 +0300

    Add initial project structure with Pipfile, Pipfile.lock, and service modules

---
+++ services/app.py
@@ -0,0 +1,59 @@
+## IMPORT DATA ##
+## LOAD DATA ##
+## SAVE DATA ##
+## CLI ##
+
+def cli_run():
+    try:
+        while True:
+            print("======CLI======")
+            print()
+            print()
+            print("1. All Products")
+            print("2. Add Product")
+            print("3. Search for Product")
+            print("4. Update Product")
+            print("5. Delete Product")
+            print("6. Save data")
+            print("7. Load data")
+            print("8. Exit")
+            print()
+            print()
+
+            choice = input("Enter a number----").strip()         
+            
+            if choice == "1":
+                pass
+
+                # ADD #
+            if choice == "2":
+                pass
+
+                # SEARCH #
+            if choice == "3":
+                pass
+
+                # UPDATE #
+            if choice == "4":
+                pass
+
+                # DELETE #
+            if choice == "5":
+                pass
+
+            if choice == "6":
+                pass
+
+            if choice == "7":
+                pass
+
+            if choice == "8":
+                pass
+
+
+    finally:
+        pass
+
+# cli_run()
+
+
